9668625d8afb1716a0cd50583147e56751211aaf
roeming <brother64youyou@gmail.com>
2025-11-30 18:23:30 -0500

    update splits + dtk version

--- configure.py
+++ configure.py
@@ -158,10 +158,10 @@ if not config.non_matching:
 # Tool versions
 config.binutils_tag = "2.42-1"
 config.compilers_tag = "20250812"
-config.dtk_tag = "v1.6.2"
-config.objdiff_tag = "v3.0.0-beta.14"
-config.sjiswrap_tag = "v1.2.1"
-config.wibo_tag = "0.7.0"
+config.dtk_tag = "v1.7.1"
+config.objdiff_tag = "v3.4.0"
+config.sjiswrap_tag = "v1.2.2"
+config.wibo_tag = "1.0.0-beta.5"
 
 # Project
 config.config_path = Path("config") / config.version / "config.yml"
