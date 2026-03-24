729f523526d01a4d5d4d430dbcd3c5d9165b305d
Michael <michael@royston.com>
2026-03-24 15:37:39 -0700

    matched fn_3_C1930

--- include/game/gap_C1930.h
+++ include/game/gap_C1930.h
@@ -3,6 +3,13 @@
 
 #include "mssbTypes.h"
 
-void fn_3_C1930(void);
+typedef struct {
+    s16 field0;
+    u8 _pad[0x1E];
+} Data17260Entry; // size: 0x20
+
+extern Data17260Entry lbl_3_data_17260[];
+
+s32 fn_3_C1930(s32 arg0);
 
 #endif // !__GAME_gap_C1930_H_
--- src/game/gap_C1930.c
+++ src/game/gap_C1930.c
@@ -1,7 +1,13 @@
 #include "game/gap_C1930.h"
 
 // .text:0x000C1930 size:0x34 mapped:0x807009C4
-void fn_3_C1930(void) {
-    return;
+s32 fn_3_C1930(s32 arg0) {
+    s32 i = 1;
+    Data17260Entry* entry = &lbl_3_data_17260[1];
+    do {
+        if (arg0 == entry->field0) break;
+        entry--;
+    } while (i--);
+    return i;
 }
 
