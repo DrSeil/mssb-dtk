8a781387dddc9e1966067cbb752c2427c09ea6f4
Michael <michael@royston.com>
2026-03-24 13:37:02 -0700

    matched fn_3_169150

--- src/game/gap_1690C0.c
+++ src/game/gap_1690C0.c
@@ -1,4 +1,5 @@
 #include "game/gap_1690C0.h"
+#include "game/rep_3E58.h"
 
 // .text:0x001690C0 size:0x90 mapped:0x807A8154
 void fn_3_1690C0(void) {
@@ -7,6 +8,6 @@ void fn_3_1690C0(void) {
 
 // .text:0x00169150 size:0x2C mapped:0x807A81E4
 void fn_3_169150(void) {
-    return;
+    fn_800B0A5C_insertQueue(fn_3_1690C0, 0);
 }
 
