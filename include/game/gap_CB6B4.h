8bf9641181e5bf5d744f4a8c8aca89aeabdb061d
Michael <michael@royston.com>
2026-03-24 16:02:11 -0700

    matched fn_3_CB6B4

--- include/game/gap_CB6B4.h
+++ include/game/gap_CB6B4.h
@@ -3,7 +3,10 @@
 
 #include "mssbTypes.h"
 
-void fn_3_CB6B4(void);
+extern u32 lbl_3_bss_9FDC;
+void convertTextureHeader(u32 arg0);
+
+void fn_3_CB6B4(u32* arg0);
 void fn_3_CB6EC(void);
 
 #endif // !__GAME_gap_CB6B4_H_
--- src/game/gap_CB6B4.c
+++ src/game/gap_CB6B4.c
@@ -1,8 +1,10 @@
 #include "game/gap_CB6B4.h"
 
 // .text:0x000CB6B4 size:0x38 mapped:0x8070A748
-void fn_3_CB6B4(void) {
-    return;
+void fn_3_CB6B4(u32* arg0) {
+    *arg0 += (u32)arg0;
+    lbl_3_bss_9FDC = *arg0;
+    convertTextureHeader(lbl_3_bss_9FDC);
 }
 
 // .text:0x000CB6EC size:0x4C mapped:0x8070A780
