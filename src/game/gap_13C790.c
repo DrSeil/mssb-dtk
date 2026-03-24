f94e0a51f49a0181080edff7e07712d86778e1f7
Michael <michael@royston.com>
2026-03-24 11:27:01 -0700

    matched fn_3_13C790

--- src/game/gap_13C790.c
+++ src/game/gap_13C790.c
@@ -1,7 +1,12 @@
 #include "game/gap_13C790.h"
+#include "game/UnknownHomes_Game.h"
 
 // .text:0x0013C790 size:0x2C mapped:0x8077B824
 void fn_3_13C790(void) {
-    return;
+    s8 i = 0;
+    do {
+        g_Minigame._1DBC[i + 4] = 0;
+        i++;
+    } while (i < 4);
 }
 
