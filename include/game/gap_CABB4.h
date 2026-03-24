5f34656ee1b3cba6f366ea5fb46a46cb9a634b2e
Michael <michael@royston.com>
2026-03-24 16:13:42 -0700

    matched fn_3_CABB4

--- include/game/gap_CABB4.h
+++ include/game/gap_CABB4.h
@@ -3,6 +3,18 @@
 
 #include "mssbTypes.h"
 
+typedef struct {
+    u8 _pad[0x34];
+    u8 unk34;
+} CabB4Object;
+
+typedef struct {
+    CabB4Object* ptr0;
+    CabB4Object* ptr4;
+} Data17D10;
+
+extern Data17D10 lbl_3_data_17D10;
+
 void fn_3_CABB4(void);
 
 #endif // !__GAME_gap_CABB4_H_
--- src/game/gap_CABB4.c
+++ src/game/gap_CABB4.c
@@ -2,6 +2,12 @@
 
 // .text:0x000CABB4 size:0x3C mapped:0x80709C48
 void fn_3_CABB4(void) {
-    return;
+    if (lbl_3_data_17D10.ptr0 != NULL) {
+        lbl_3_data_17D10.ptr0->unk34 = 1;
+    }
+    if (lbl_3_data_17D10.ptr4 == NULL) {
+        return;
+    }
+    lbl_3_data_17D10.ptr4->unk34 = 1;
 }
 
