5d6e47b925402177b7e7616a0497e437b6ebca6f
Michael <michael@royston.com>
2026-03-25 13:20:22 -0700

    matched _prolog

--- include/game/gap_464.h
+++ include/game/gap_464.h
@@ -5,6 +5,17 @@
 #include "static/UnknownHomes_Static.h"
 #include "game/rep_1E08.h"
 
+typedef struct { u8 _pad[0x5C]; } Data118Sub;
+typedef struct {
+    u8 _pad0[0x5C];
+    Data118Sub sub;
+} Data118;
+
+extern Data118 lbl_3_data_118;
+void fn_80036C88(Data118* arg0, Data118Sub* arg1);
+void fn_800B0D28(Data118Sub* arg0);
+void fn_8004B270(void);
+
 void fn_800BF038(s32 arg0, GameInitVariables* arg1);
 void _epilog(void);
 void _prolog(void);
--- src/game/gap_464.c
+++ src/game/gap_464.c
@@ -1,5 +1,8 @@
 #include "game/gap_464.h"
 
+void fn_3_5AE9C(void);
+void fn_3_C0824(void);
+
 // .text:0x00000464 size:0x38 mapped:0x8063F4F8
 void _epilog(void) {
     g_d_GameSettings._55 = 0;
@@ -9,7 +12,11 @@ void _epilog(void) {
 
 // .text:0x0000049C size:0x58 mapped:0x8063F530
 void _prolog(void) {
-    return;
+    fn_800B0A5C_insertQueue(fn_3_5AE9C, 1);
+    fn_3_C0824();
+    fn_80036C88(&lbl_3_data_118, &lbl_3_data_118.sub);
+    fn_800B0D28(&lbl_3_data_118.sub);
+    fn_8004B270();
 }
 
 // .text:0x000004F4 size:0x168 mapped:0x8063F588
