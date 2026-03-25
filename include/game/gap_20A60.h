cf64bf881f6a51e4628ade89056cb113cde0b879
Michael <michael@royston.com>
2026-03-25 12:52:55 -0700

    matched fn_3_20A60; almost matched fn_3_20AE0
    fn_3_20AE0: f0/f2 float register swap for rodata_930/rodata_938; all other logic correct

--- include/game/gap_20A60.h
+++ include/game/gap_20A60.h
@@ -2,6 +2,11 @@
 #define __GAME_gap_20A60_H_
 
 #include "mssbTypes.h"
+#include "game/UnknownHomes_Game.h"
+
+extern f32 lbl_3_rodata_930;
+extern f32 lbl_3_rodata_938;
+extern f32 lbl_3_rodata_93C;
 
 void fn_3_20A60(void);
 void fn_3_20AB0(void);
--- src/game/gap_20A60.c
+++ src/game/gap_20A60.c
@@ -3,7 +3,13 @@
 
 // .text:0x00020A60 size:0x50 mapped:0x8065FAF4
 void fn_3_20A60(void) {
-    return;
+    g_AiLogic.boxHorizontalPoint = g_Batter.batPosition2.x;
+    g_AiLogic.boxVerticalPoint = g_Batter.batPosition2.z;
+    g_AiLogic.batterAIDesiredZPosInBox = maybeInitialBatPos.z;
+    g_AiLogic.batterAIABStrat = -1;
+    g_AiLogic._67 = 0;
+    g_AiLogic.batterAIBuntPossibility = 1;
+    g_AiLogic.batterAIBuntInd = 0;
 }
 
 // .text:0x00020AB0 size:0x30 mapped:0x8065FB44
@@ -19,7 +25,18 @@ void fn_3_20AB0(void) {
 
 // .text:0x00020AE0 size:0x50 mapped:0x8065FB74
 void fn_3_20AE0(void) {
-    return;
+    f32 r938 = lbl_3_rodata_938;
+    f32 z = maybeInitialBatPos.z;
+    f32 x = lbl_3_rodata_930;
+    f32 r93C = lbl_3_rodata_93C;
+    g_AiLogic.batterAIDesiredXPosInBox = x;
+    g_AiLogic.batterAIDesiredZPosInBox = z;
+    g_AiLogic._10 = r938;
+    g_AiLogic._14 = r93C;
+    g_AiLogic._18 = r938;
+    g_AiLogic._1C = r93C;
+    g_AiLogic.batterAIDesiredXPosInBox = x;
+    g_AiLogic.batterAIDesiredZPosInBox = z;
 }
 
 // .text:0x00020B30 size:0x104 mapped:0x8065FBC4
