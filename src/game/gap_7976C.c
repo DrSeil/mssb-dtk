494e8623541170b4d85d544a4f34e263cf4930d3
Michael <michael@royston.com>
2026-03-28 21:12:34 -0700

    Almost matched fn_3_7AF68
    Identical pattern to fn_3_7AEEC: CW running-pointer on bss struct not reproducible from C struct field access

--- src/game/gap_7976C.c
+++ src/game/gap_7976C.c
@@ -87,7 +87,26 @@ void fn_3_7AEEC(void) {
 
 // .text:0x0007AF68 size:0x7C mapped:0x806B9FFC
 void fn_3_7AF68(void) {
-    return;
+    InMemRunnerType *pRunners = g_Runners;
+    CommonBss32A94 *pBss = &lbl_3_common_bss_32A94;
+    pBss->_pad6[0x1C] = (u8)-1;
+    pBss->_pad6[0x21] = 0;
+    if (pRunners[1].runnerOnFieldOrOutOrScored != 0) {
+        pBss->_pad6[0x22] = 1;
+    } else {
+        pBss->_pad6[0x22] = 0;
+    }
+    pRunners += 2;
+    if (pRunners[0].runnerOnFieldOrOutOrScored != 0) {
+        pBss->_pad6[0x23] = 1;
+    } else {
+        pBss->_pad6[0x23] = 0;
+    }
+    if (pRunners[1].runnerOnFieldOrOutOrScored != 0) {
+        pBss->_pad6[0x24] = 1;
+    } else {
+        pBss->_pad6[0x24] = 0;
+    }
 }
 
 // .text:0x0007AFE4 size:0x14C mapped:0x806BA078
