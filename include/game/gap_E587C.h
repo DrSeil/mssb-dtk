f531ae34c654fcf2d4d3edd19838faf937e957e1
Michael <michael@royston.com>
2026-04-02 00:08:48 -0700

    matched fn_3_E587C

--- include/game/gap_E587C.h
+++ include/game/gap_E587C.h
@@ -3,7 +3,7 @@
 
 #include "mssbTypes.h"
 
-void fn_3_E587C(void);
+s32 fn_3_E587C(void);
 s32 fn_3_E5924(void);
 
 #endif // GAME_GAP_E587C_H
--- src/game/gap_E587C.c
+++ src/game/gap_E587C.c
@@ -5,8 +5,18 @@
 extern u8 lbl_3_data_196B4[];
 
 // .text:0x000E587C size:0xA8
-void fn_3_E587C(void) {
-    return;
+s32 fn_3_E587C(void) {
+    s32 count = 0;
+    s32 last;
+    s32 i;
+    for (i = 0; i < 7; i++) {
+        if (lbl_3_data_196B4[i] != 0) {
+            last = i;
+            count++;
+        }
+    }
+    if (count != 1) return -1;
+    return last;
 }
 
 // .text:0x000E5924 size:0x90
