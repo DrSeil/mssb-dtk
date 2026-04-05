002b00f4660b7445ddea3173acb2461a8a5bf3d3
Michael <michael@royston.com>
2026-04-05 12:25:33 -0700

    matched fn_3_6D658

--- include/game/gap_6D4A0.h
+++ include/game/gap_6D4A0.h
@@ -5,8 +5,22 @@
 #include "game/UnknownHomes_Game.h"
 #include "static/UnknownHomes_Static.h"
 
+typedef struct {
+    u8 _pad0[0x3B]; // 0x00
+    u8 stats[0x65]; // 0x3B
+} Struct_8034E9A0;  // 0xA0 bytes
+
+typedef struct {
+    u8 _pad0[0x28]; // 0x00
+    f32 unk_28;     // 0x28
+    u8 _pad1[0x4];  // 0x2C
+} Struct_5FC4;      // 0x30 bytes
+
+extern Struct_8034E9A0 lbl_8034E9A0[];
+extern Struct_5FC4 lbl_3_data_5FC4;
+
 void fn_3_6D4A0(void);
 void fn_3_6D564(void);
-void fn_3_6D658(void);
+s32 fn_3_6D658(s32 arg0, s32 arg1, s32 arg2);
 
 #endif // !__GAME_gap_6D4A0_H_
--- src/game/gap_6D4A0.c
+++ src/game/gap_6D4A0.c
@@ -22,7 +22,18 @@ BOOL checkFieldingStat(int teamIdx, int playerIdx, int statBit) {
 }
 
 // .text:0x0006D658 size:0x7C mapped:0x806AC6EC
-void fn_3_6D658(void) {
-    return;
+s32 fn_3_6D658(s32 arg0, s32 arg1, s32 arg2) {
+    s32 val;
+
+    val = lbl_8034E9A0[arg1].stats[arg2];
+    if (g_d_GameSettings.exhibitionMatchInd == 0 &&
+        g_d_GameSettings.humanTeamNumber == arg0 &&
+        (s8)g_d_GameSettings.challengeCaptainStarBought[5] != 0) {
+        val += (s32)lbl_3_data_5FC4.unk_28;
+    }
+    if (val > 100) {
+        val = 100;
+    }
+    return val;
 }
 
